558904b6f39fda6dd2ae70c09c4bcd4f8cb61952
Antonio <antoniomf.fp@gmail.com>
2023-01-18 19:42:54 +0100

    primer commit

---
+++ EFconASPyMVC/Models/Alumno.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFconASPyMVC.Models
+{
+    public class Alumno
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required, MaxLength(30), MinLength(3)]
+        public string Nombre { get; set; }
+        public int Edad { get; set; }
+
+        //Clave foránea con respecto a Clase
+        public int ClaseId { get; set; }
+
+        //propiedad navegacional
+        public Usuario Usuario { get; set; }
+
+        //propiedad navegacional
+        public List<AlumnoTareas> AlumnoTareas { get; set; }
+
+        //propiedad navegacional 
+        public Clase Clase { get; set; }
+    }
+}
---
+++ EFconASPyMVC/Models/Usuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFconASPyMVC.Models
+{
+    public class Usuario
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required, MaxLength(15), MinLength(4)]
+        public string NombreUsuario { get; set; }
+
+        public int AlumnoId { get; set; }
+
+        public Alumno Alumno { get; set; }
+    }
+}
